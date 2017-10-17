a0c6eca4aedbff203e4a3c1153a6e3d1271a19bd
cyrilforrez <cyilforrez@gmail.com>
2017-10-17 16:38:08 +0200

    correction

--- exo4/index.php
+++ exo4/index.php
@@ -2,62 +2,60 @@
 
 class form
 {
-  private $_form;
-  private $_text;
-  private $_fieldset;
-  private $_submit;
+    private $_form;
+    private $_text;
+    private $_fieldset;
+    private $_submit;
 
-  public function __construct()
-  {
-    $this->setFieldset();
-    $this->setText();
-    $this->setText();
-    $this->setSubmit();
-    $this->setForm();
-  }
+    public function __construct()
+    {
+        $this->setFieldset();
+        $this->setText();
+        $this->setText();
+        $this->setSubmit();
+        $this->setForm();
+    }
 
-  public function getForm()
-  {
-    echo $this->getFieldset(), $this->getText(), $this->getSubmit();
-  }
+    public function getForm()
+    {
+        echo $this->getFieldset(), $this->getText(), $this->getSubmit();
+    }
 
-  public function getText()
-  {
-    return $this->_text;
-  }
+    public function getText()
+    {
+        return $this->_text;
+    }
 
-  public function getFieldset()
-  {
-    return $this->_fieldset;
-  }
+    public function getFieldset()
+    {
+        return $this->_fieldset;
+    }
 
-  public function getSubmit()
-  {
-    return $this->_submit;
-  }
+    public function getSubmit()
+    {
+        return $this->_submit;
+    }
 
-  public function setText()
-  {
-    $this->_text = '<br /><label>Entrez votre pseudo</label><input type=text />';
-  }
+    public function setText()
+    {
+        $this->_text = '<br /><label>Entrez votre pseudo</label><input type=text />';
+    }
 
-  public function setFieldset()
-  {
-    $this->_fieldset = '<form methode=post><fieldset>Bonjour';
-  }
+    public function setFieldset()
+    {
+        $this->_fieldset = '<form methode=post><fieldset>Bonjour';
+    }
 
-  public function setSubmit()
-  {
-    $this->_submit = '<input type=submit />';
-  }
+    public function setSubmit()
+    {
+        $this->_submit = '<input type=submit />';
+    }
 
-  public function setForm()
-  {
-    $this->_form = ' </fieldset></form>';
-  }
+    public function setForm()
+    {
+        $this->_form = ' </fieldset></form>';
+    }
 }
 
 $form1 = new form;
 $form1->getForm();
-
-?>
